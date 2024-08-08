6e4a69c19e5dbc74d8960a93bec9574428da8f05
Rudra Pal <Rudra Pal@DESKTOP-EPCRK2D>
2024-08-08 18:34:57 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Dropdownbinding.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Dropdownbinding.Controllers
+{
+    public class HomeController : Controller
+    {
+        JquerdataEntities1 db = new JquerdataEntities1 ();
+        public ActionResult Index()
+        {
+            List<loading> namelist  = db.loadings.ToList ();
+            ViewBag.nametbl = new  SelectList(namelist, "Id","Name");
+            return View();
+        }
+
+        
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Dropdownbinding.MvcApplication" Language="C#" %>
